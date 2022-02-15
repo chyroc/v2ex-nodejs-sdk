a669ebb78e4268119a80bc2c9432c7645eb1299f
chyroc <chyroc@qq.com>
2022-02-15 19:25:12 +0800

    getProfile

--- src/api.ts
+++ src/api.ts
@@ -4,6 +4,17 @@
 import axios, {AxiosInstance} from 'axios';
 
 
+export interface Member {
+  id: number;
+  username: string;
+  bio: string;
+  website: string;
+  github: string;
+  url: string;
+  avatar: string;
+  created: number;
+}
+
 export interface Notification {
   id: number;
   member_id: number;
@@ -12,15 +23,95 @@ export interface Notification {
   payload: null | string;
   payload_rendered: string;
   created: number;
-  member: {
-    username: string;
-  };
+  member: Member;
+}
+
+export interface Profile {
+  id: number;
+  username: string;
+  url: string;
+  website: string;
+  twitter: string;
+  psn: string;
+  github: string;
+  btc: string;
+  location: string;
+  tagline: string;
+  bio: string;
+  avatar_mini: string;
+  avatar_normal: string;
+  avatar_large: string;
+  created: number;
+  last_modified: number;
+}
+
+export interface Token {
+  token: string;
+  scope: string;
+  expiration: number;
+  good_for_days: number;
+  total_used: number;
+  last_used: number;
+  created: number;
+}
+
+export enum TokenExpiration {
+  D30 = 2592000,
+  D60 = 5184000,
+  D90 = 7776000,
+  D180 = 15552000,
+}
+
+export enum TokenScope {
+  everything = 'everything',
+  regular = 'regular'// 如果是 regular 类型的 Token 将不能用于进一步创建新的 token
+}
+
+export interface Node {
+  id: number;
+  url: string;
+  name: string;
+  title: string;
+  header: string;
+  footer: string;
+  avatar: string;
+  topics: number;
+  created: number;
+  last_modified: number;
+}
+
+export interface Topic {
+  id: number;
+  title: string;
+  content: string;
+  content_rendered: string;
+  syntax: number;
+  url: string;
+  replies: number;
+  last_reply_by: string;
+  created: number;
+  last_modified: number;
+  last_touched: number;
+  member: Member;
+  node: Node;
+  supplements: any[];
+}
+
+export interface TopicReply {
+  id: number;
+  content: string;
+  content_rendered: string;
+  created: number;
+  member: Member;
 }
 
 export default class V2EX {
   axios: AxiosInstance
 
-  constructor(token: string, timeout: number = 1000) {
+  constructor(token: string, config: {
+    timeout: number,
+  }) {
+    const timeout = config.timeout || 3000
     this.axios = axios.create({
       baseURL: 'https://www.v2ex.com/api/v2/',
       timeout,
@@ -53,25 +144,52 @@ export default class V2EX {
     total: number
   }> {
     const resp = await this.axios.get(`notifications?p=${page}`)
-    const {
-      result,
-      message
-    } = resp.data as { result: Notification[], message: string }
-
-
+    const {result, message} = resp.data as { result: Notification[], message: string }
     let total = result && result.length || 0
     try {
       total = +message.split('/', 2)[1]
     } catch (e) {
       //
     }
-    return {
-      notifications: result,
-      total,
-    }
+    return {notifications: result, total,}
   }
 
   public async deleteNotification(id: number): Promise<void> {
     await this.axios.delete(`notifications/${id}`)
+  }
+
+  public async getProfile(): Promise<Profile> {
+    const resp = await this.axios.get(`member`)
+    return resp.data.result as Profile
+  }
+
+  public async getToken(): Promise<Token> {
+    const resp = await this.axios.get(`token`)
+    return resp.data.result as Token
+  }
+
+  public async createToken(req: { scope: TokenScope, expiration: TokenExpiration }): Promise<string> {
+    const resp = await this.axios.post(`tokens`, req)
+    return resp.data.result.token as string
+  }
+
+  public async getNode(name: string): Promise<Node> {
+    const resp = await this.axios.get(`nodes/${name}`)
+    return resp.data.result as Node
+  }
+
+  public async getTopicByNode(node: string, page: number = 1): Promise<Topic[]> {
+    const resp = await this.axios.get(`nodes/${node}/topics?p=${page}`)
+    return resp.data.result as Topic[]
+  }
+
+  public async getTopic(id: number): Promise<Topic> {
+    const resp = await this.axios.get(`topics/${id}`)
+    return resp.data.result as Topic
+  }
+
+  public async getTopicReply(topic: number, page: number = 1): Promise<TopicReply[]> {
+    const resp = await this.axios.get(`topics/${topic}/replies?p=${page}`)
+    return resp.data.result as TopicReply[]
   }
 }
