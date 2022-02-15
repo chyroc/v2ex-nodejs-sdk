eaabe08538fdd350b70c29a7ab71aa1e8c02c343
chyroc <chyroc@qq.com>
2022-02-15 18:40:45 +0800

    getNotifications

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,50 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+// import "core-js/fn/array.find"
+// ...
+import axios, {AxiosInstance} from 'axios';
 
+
+export interface Notification {
+  id: number;
+  member_id: number;
+  for_member_id: number;
+  text: string;
+  payload: null | string;
+  payload_rendered: string;
+  created: number;
+  member: {
+    username: string;
+  };
+}
+
+export default class V2EX {
+  axios: AxiosInstance
+
+  constructor(token: string, timeout: number = 1000) {
+    this.axios = axios.create({
+      baseURL: 'https://www.v2ex.com/api/v2/',
+      timeout,
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+  }
+
+  public async getNotifications(page: number = 1): Promise<{
+    notifications: Notification[]
+    total: number
+  }> {
+    const resp = await this.axios.get(`notifications?p=${page}`)
+    const {result, message} = resp.data as { result: Notification[], message: string }
+    let total = result && result.length || 0
+    try {
+      total = +message.split('/', 2)[1]
+    } catch (e) {
+      //
+    }
+    return {
+      notifications: result,
+      total,
+    }
+  }
 }
---
+++ test/api.ts
@@ -0,0 +1,19 @@
+import V2EX from "../src/api"
+
+/**
+ * Dummy test
+ */
+describe("V2EX test", () => {
+  it("works if true is truthy", () => {
+    expect(true).toBeTruthy()
+  })
+
+  it("V2EX is instantiable", () => {
+    expect(new V2EX('token')).toBeInstanceOf(V2EX)
+  })
+
+  it("getNotifications", async () => {
+    const token = process.env.V2EX_TOKEN || ''
+    expect(await new V2EX(token).getNotifications()).toBeInstanceOf(Promise)
+  })
+})
